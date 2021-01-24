4ea0eebbe1001cc42304642b1ab33f7bcce65fc4
Semilore.omoyeleomoyinmi <john.omoyinmi@gmail.com>
2021-01-24 15:48:09 -0500

    added changes

--- lab01/lab01.java
+++ lab01/lab01.java
@@ -3,5 +3,6 @@ public class lab01 {
     public static void main( String[] args)
     {
         System.out.println("Hello world");
+	System.out.println("new change");// new changes made to the cloned repository
     }
 }
