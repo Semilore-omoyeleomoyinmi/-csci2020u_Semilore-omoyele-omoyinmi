1ecafed839d581298207b174f05eee2bfb4cdb74
Semilore.omoyeleomoyinmi <john.omoyinmi@gmail.com>
2021-01-24 19:43:13 -0500

    new changes in master branch

--- lab01/lab01.java
+++ lab01/lab01.java
@@ -4,5 +4,6 @@ public class lab01 {
     {
         System.out.println("Hello world");
 	System.out.println("new change");// new changes made to the cloned repository
+	System.out.println("change in master branch"); // change in master branch
     }
 }
