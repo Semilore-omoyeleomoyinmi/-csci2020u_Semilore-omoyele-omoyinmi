0be3e8ed5be8415df78833c6e7c175855234103e
Semilore.omoyeleomoyinmi <john.omoyinmi@gmail.com>
2021-01-24 19:29:28 -0500

    made new branch with changes to file

--- lab01/lab01.java
+++ lab01/lab01.java
@@ -4,5 +4,6 @@ public class lab01 {
     {
         System.out.println("Hello world");
 	System.out.println("new change");// new changes made to the cloned repository
+	System.out.println("new change in branch beta");// new changes made to the branch
     }
 }
