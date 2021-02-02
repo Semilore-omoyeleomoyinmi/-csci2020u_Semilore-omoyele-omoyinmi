d5c47af8874e96a4778b7c1411ee4f39febed3f8
Semilore.omoyeleomoyinmi <john.omoyinmi@gmail.com>
2021-02-01 22:06:32 -0500

    lab02

---
+++ lab02/csci2020u/lab02/src/main/java/csci2020u/lab02/SimpleTest.java
@@ -0,0 +1,8 @@
+package csci2020u.lab02;
+import java.util.Arrays;
+public class SimpleTest {
+    public static void main(String [] args)
+    {
+        System.out.println("Hello Simple test");
+    }
+}
